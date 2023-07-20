7e05ccadd70952d9e059d3ab122079dda6d347d8
Bogdan21o312 <lutskiv2010@gmail.com>
2023-07-20 15:25:41 +0300

    Create common lib

---
+++ libs/common/src/index.ts
@@ -0,0 +1,11 @@
+// Entites
+export {EntityContainer} from './entites';
+// Shared
+export {
+  // UI
+  Wrapper,
+  Ibg,
+  ModalWindow,
+  // Icons
+  IconSearch, IconBookmark, IconPhone, IconCart, IconClose
+} from './shared';
---
+++ libs/common/src/shared/index.ts
@@ -0,0 +1,6 @@
+// UI
+export {Ibg, Wrapper, ModalWindow} from './UI';
+// Types
+export {TypeIbg} from './types';
+// Icons
+export {IconSearch, IconCart, IconPhone, IconBookmark, IconClose} from "./assets"
