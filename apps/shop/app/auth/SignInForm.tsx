2cf051f3eb7cf6aeb214eb5d1e88043065e94363
Bogdan21o312 <lutskiv2010@gmail.com>
2023-08-06 12:54:03 +0300

    Update

--- apps/shop/app/auth/SignInForm.tsx
+++ apps/shop/app/auth/SignInForm.tsx
@@ -1,45 +1,45 @@
-"use client";
+'use client'
 
-import { useRouter } from "next/navigation";
+import { useRouter } from 'next/navigation'
 import { signIn, signOut, useSession } from 'next-auth/react'
-import type { FormEventHandler } from "react";
+import type { FormEventHandler } from 'react'
 
 const SignInForm = () => {
-  const router = useRouter();
-  const { data: session } = useSession(); // Витягуємо інформацію про сесію
+  const router = useRouter()
+  const { data: session } = useSession() // Витягуємо інформацію про сесію
 
   const handleSubmit: FormEventHandler<HTMLFormElement> = async (event) => {
-    event.preventDefault();
+    event.preventDefault()
 
-    const formData = new FormData(event.currentTarget);
+    const formData = new FormData(event.currentTarget)
 
-    const res = await signIn("credentials", {
-      email: formData.get("email"),
-      password: formData.get("password"),
-      redirect: false,
-    });
+    const res = await signIn('credentials', {
+      email: formData.get('email'),
+      password: formData.get('password'),
+      redirect: false
+    })
 
     if (res && !res.error) {
-      router.push("/");
+      router.push('/')
     } else {
-      console.log(res);
+      console.log(res)
     }
-  };
+  }
   const handleLogout = () => {
-    signOut(); // Викликаємо функцію розлогінування
-  };
+    signOut() // Викликаємо функцію розлогінування
+  }
   return (
     <div>
-      <form onSubmit={handleSubmit} className="login-form">
-        <input type="email" name="email" required />
-        <input type="password" name="password" required />
-        <button type="submit">Sign In</button>
+      <form onSubmit={handleSubmit} className='login-form'>
+        <input type='email' name='email' required />
+        <input type='password' name='password' required />
+        <button type='submit'>Sign In</button>
       </form>
       <button onClick={handleLogout} disabled={!session}>
         Logout
       </button>
     </div>
-  );
-};
+  )
+}
 
-export { SignInForm };
+export { SignInForm }
