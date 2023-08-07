f063d7048b8daf81f26d15f5b342f248a89ee9e4
Bogdan21o312 <lutskiv2010@gmail.com>
2023-08-08 00:40:36 +0300

    Update

--- apps/admin-panel/app/fdsakljfdks.tsx
+++ apps/admin-panel/app/fdsakljfdks.tsx
@@ -3,58 +3,68 @@
 import { useState } from 'react'
 
 export function Ant() {
-  const [phone, setPhone] = useState('');
+  const [phone, setPhone] = useState('')
 
   const getInputNumbersValue = (input) => {
-    return input.replace(/\D/g, '');
+    return input.replace(/\D/g, '')
   }
 
   const onPhonePaste = (e) => {
-    e.preventDefault();
-    const input = e.target;
-    const inputNumbersValue = getInputNumbersValue(e.clipboardData.getData('Text'));
+    e.preventDefault()
+    const input = e.target
+    const inputNumbersValue = getInputNumbersValue(e.clipboardData.getData('Text'))
 
     if (inputNumbersValue) {
-      setPhone(formatPhoneNumber(inputNumbersValue));
+      setPhone(formatPhoneNumber(inputNumbersValue))
     }
   }
 
   const formatPhoneNumber = (inputNumbersValue) => {
-    let formattedValue = '';
+    let formattedValue = ''
 
     if (['7', '8', '9'].includes(inputNumbersValue[0])) {
-      if (inputNumbersValue[0] === '9') inputNumbersValue = '7' + inputNumbersValue;
-      formattedValue = '+7 ';
+      if (inputNumbersValue[0] === '9') inputNumbersValue = '7' + inputNumbersValue
+      formattedValue = '+7 '
       if (inputNumbersValue.length > 1) {
-        formattedValue += '(' + inputNumbersValue.substring(1, 4);
+        formattedValue += '(' + inputNumbersValue.substring(1, 4)
       }
       if (inputNumbersValue.length >= 5) {
-        formattedValue += ') ' + inputNumbersValue.substring(4, 7);
+        formattedValue += ') ' + inputNumbersValue.substring(4, 7)
       }
       if (inputNumbersValue.length >= 8) {
-        formattedValue += '-' + inputNumbersValue.substring(7, 9);
+        formattedValue += '-' + inputNumbersValue.substring(7, 9)
       }
       if (inputNumbersValue.length >= 10) {
-        formattedValue += '-' + inputNumbersValue.substring(9, 11);
+        formattedValue += '-' + inputNumbersValue.substring(9, 11)
       }
     } else {
-      formattedValue = '+' + inputNumbersValue.substring(0, 16);
+      formattedValue = '+' + inputNumbersValue.substring(0, 16)
     }
 
-    return formattedValue;
+    return formattedValue
   }
 
   const onPhoneInput = (e) => {
-    const input = e.target;
-    const inputNumbersValue = getInputNumbersValue(input.value);
-    const formattedPhoneNumber = formatPhoneNumber(inputNumbersValue);
+    const input = e.target
+    const selectionStart = input.selectionStart
 
-    setPhone(formattedPhoneNumber);
+    if (input.value.length !== selectionStart) {
+      const inputNumbersValue = getInputNumbersValue(input.value)
+      setPhone(inputNumbersValue)
+
+      input.setSelectionRange(selectionStart, selectionStart)
+      return
+    }
+
+    const inputNumbersValue = getInputNumbersValue(input.value)
+    const formattedPhoneNumber = formatPhoneNumber(inputNumbersValue)
+
+    setPhone(formattedPhoneNumber)
   }
 
   const onPhoneKeyDown = (e) => {
     if (e.key === 'Backspace' && phone.length === 1) {
-      setPhone('');
+      setPhone('')
     }
   }
 
@@ -70,5 +80,5 @@ export function Ant() {
         onPaste={onPhonePaste}
       />
     </div>
-  );
+  )
 }
