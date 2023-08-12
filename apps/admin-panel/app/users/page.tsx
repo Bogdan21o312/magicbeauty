7642993ff7cf05b7ea66fe1ecf842cd5b80349b6
Bogdan21o312 <lutskiv2010@gmail.com>
2023-08-12 22:10:24 +0300

    Create server serach

--- apps/admin-panel/app/users/page.tsx
+++ apps/admin-panel/app/users/page.tsx
@@ -1,5 +1,4 @@
 import { EntityFormCreateUser, FeatureTableUsers } from '@magicbeauty/common/server'
-import { InputWithQueryParam } from '../../../../libs/common/src/features/FeatureTableUsers/InputWithQueryParam'
 
 export default async function Page() {
 
@@ -7,8 +6,7 @@ export default async function Page() {
 
   return (
     <div>
-      <InputWithQueryParam/>
-      {/*<EntityFormCreateUser/>*/}
+      <EntityFormCreateUser/>
       <FeatureTableUsers/>
     </div>
   )
--- libs/common/src/features/FeatureTableUsers/FeatureTableUsers.tsx
+++ libs/common/src/features/FeatureTableUsers/FeatureTableUsers.tsx
@@ -1,60 +1,67 @@
 import classes from './FeatureTableUsers.module.scss'
 import { deleteUser, prisma } from '../../actions'
 import { TableUser } from './components'
-import { A } from './InputWithQueryParam'
+import { cookies } from 'next/headers'
+import { revalidatePath } from 'next/cache'
 
 async function deleteUserAction(id: number) {
   'use server'
   deleteUser(id)
 }
+async function search(data: FormData) {
+  'use server'
+  const cookieStore = cookies()
+  cookieStore.set('name', data.get('search') as string)
+  revalidatePath('/users')
+}
 
-export async function FeatureTableUsers() {
-  let value = 'f'
-
-  // function search(data: FormData) {
-  //   const valueValid = data.get('search') as string
-  //   const a = value + valueValid
-  //   console.log(a)
-  // }
-
+async function getAllUsersBySearch() {
+  'use server'
+  const cookieStore = cookies()
+  const name = cookieStore.get('name')?.value
+  console.log(name)
   const users = await prisma.user.findMany({
     where: {
       firstName: {
-        contains: value,
+        contains: name,
         mode: 'insensitive'
       }
     }
   })
+  return users
+}
+
+export async function FeatureTableUsers() {
+  const users = await getAllUsersBySearch()
 
   return (
     <div>
-      {/*<form action={search}>*/}
-      {/*  <input type='text' name='search' placeholder='Search' />*/}
-      {/*</form>*/}
-      {/*<A search={search} />*/}
+      <form action={search}>
+        <input type='text' name='search' placeholder='Search' />
+      </form>
       <table className={classes.table}>
         <tbody>
-        {users.map((user, index) =>
-          <TableUser
-            key={user.id}
-            id={user.id}
-            email={user.email}
-            password={user.password}
-            phone={user.phone}
-            firstName={user.firstName}
-            surname={user.surname}
-            patronymic={user.patronymic}
-            country={user.country}
-            region={user.region}
-            settlement={user.settlement}
-            avatar={user.avatar}
-            role={user.role}
-            createdAt={user.createdAt}
-            updatedAt={user.updatedAt}
-            index={index + 1}
-            deleteUser={deleteUserAction}
-          />
-        )}
+          {users.map((user, index) =>
+            <TableUser
+              key={user.id}
+              id={user.id}
+              email={user.email}
+              password={user.password}
+              phone={user.phone}
+              firstName={user.firstName}
+              surname={user.surname}
+              patronymic={user.patronymic}
+              country={user.country}
+              region={user.region}
+              settlement={user.settlement}
+              avatar={user.avatar}
+              role={user.role}
+              createdAt={user.createdAt}
+              updatedAt={user.updatedAt}
+              index={index + 1}
+              deleteUser={deleteUserAction}
+            />
+          )}
         </tbody>
       </table>
     </div>
